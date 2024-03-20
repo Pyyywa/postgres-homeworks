850e873fc28cc8cdea9ad7a90cca0a5653988e72
Rusha <pyyywa@gmail.com>
2024-03-20 20:58:08 +0300

    homework3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,16 +1,37 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
+SELECT c.company_name, e.first_name, e.last_name
+FROM orders as o
+JOIN customers as c USING(customer_id)
+JOIN employees as e USING(employee_id)
+JOIN shippers as s ON o.ship_via = s.shipper_id
+WHERE c.city = 'London' AND e.city = 'London' AND s.company_name = 'United Package';
 
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT p.product_name, p.units_in_stock, s.contact_name, s.phone
+FROM products as p
+JOIN suppliers as s USING(supplier_id)
+JOIN categories as c USING(category_id)
+WHERE p.discontinued = '0' AND c.category_name IN ('Dairy Products', 'Condiments') AND p.units_in_stock < 25
+ORDER BY p.units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT c.company_name, o.customer_id
+FROM orders as o
+RIGHT JOIN customers as c USING(customer_id)
+WHERE o.customer_id is NULL
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name
+FROM products as p
+WHERE EXISTS (
+	SELECT *
+	FROM order_details as od
+	WHERE p.product_id=od.product_id AND od.quantity = 10
+)
